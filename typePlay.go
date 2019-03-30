10451780a9dce732dea133237513c5667b1509d2
jackkeck <jackkeck3@gmail.com>
2019-03-30 13:53:03 -0400

    Learning types, creating my own type hotdog, and conversion (not casting)

---
+++ typePlay.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var a int
+
+type hotdog int
+
+var b hotdog
+
+func main() {
+	a = 30
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	b = 43
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+}
